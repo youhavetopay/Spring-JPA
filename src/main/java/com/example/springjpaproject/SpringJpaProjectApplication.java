74eac0ea830f44f5283b3afebaa6399c91d43edc
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-04-28 00:12:38 +0900

    init Project

---
+++ src/main/java/com/example/springjpaproject/SpringJpaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringJpaProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springjpaproject/SpringJpaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

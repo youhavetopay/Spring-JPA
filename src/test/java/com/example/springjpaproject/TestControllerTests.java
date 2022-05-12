5cc17cb44e927fd1194067ac36c53ced3705c6fd
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-05-13 00:11:59 +0900

    /test api 테스트 코드 작성
    이렇게 하는게 맞나? ㅋㅋㅋ
    
    Mock을 통해 TestController 주입 및 테스트
    assertEquals를 통해 비교

--- src/test/java/com/example/springjpaproject/TestControllerTests.java
+++ src/test/java/com/example/springjpaproject/TestControllerTests.java
@@ -1,20 +1,42 @@
 package com.example.springjpaproject;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class TestControllerTests {
 
-    @Autowired
+    @InjectMocks
+    private TestController testController;
+
     MockMvc mvc;
+
+    @BeforeEach
+    public void init(){
+        mvc = MockMvcBuilders.standaloneSetup(testController).build();
+    }
 
     @Test
     @DisplayName("/test api 테스트")
-    public void  getTestResult(){
+    public void  getTestResult() throws Exception {
+
+        MvcResult result  = mvc.perform(
+                MockMvcRequestBuilders.get("/test")
+        ).andReturn();
+
+        assertEquals("Hello Docker World", result.getResponse().getContentAsString());
 
     }
 }

e06c798bf074ef230d99e91dba7944e04a788fc0
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-06-02 23:58:21 +0900

    vue watch, computed, method 차이점 공부
    watch : 특정값이 변경될때 실행
    computed : 어떤 값을 계산해서 반환할때 ( getter느낌 )
    method : 그냥 함수

--- Web/my-front/src/App.vue
+++ Web/my-front/src/App.vue
@@ -4,6 +4,13 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <input v-model="userInfo.userId" type="text">
     <input v-model="userInfo.userPw" type="text">
+
+    <input v-model="answer" type="text">
+
+    <p>{{ inputCheckMsg }}</p>
+    <p>{{ addAnswerIzone }}</p>
+
+    <button @click="clickInputEndButton" >입력종료</button>
     <LoginComponent v-bind="userInfo"></LoginComponent>
   </div>
 
@@ -25,9 +32,27 @@ export default {
   },
   data(){
     return {
-      userInfo
+      userInfo,
+      inputCheckMsg:'입력 안하고 있음',
+      answer : ''  
     }
-  }
+  },
+  watch:{ // watch는 특정값이 변경될때 실행
+    answer: function(newVal, oldVal){
+      this.inputCheckMsg = oldVal +  ' 에서 '+ newVal +' 으로 변경 중'
+    }
+  },
+  computed:{ // computed는 그냥 어떤값을 계산해서 반환함
+    addAnswerIzone: function(){
+      return this.answer + ' 아이즈원 짱짱맨';
+    }
+  },
+  methods: { // 이건 그냥 함수
+    clickInputEndButton:function(){
+      this.inputCheckMsg = '입력 끝!!';
+    }
+  },
+
 }
 </script>
 

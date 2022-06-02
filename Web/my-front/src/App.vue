0d869e027b499a2443c915413e56ad2adf87190b
youhavetopay <58547919+youhavetopay@users.noreply.github.com>
2022-06-02 23:30:40 +0900

    vue 간단한 props 전달 구현
    일단 vue에 대해 공부를 하고 개발을 하는게 좋을듯 함
    
    시간되면 Spring으로 먼저 로그인 관련 기능구현 하는것도 좋을 듯함

--- Web/my-front/src/App.vue
+++ Web/my-front/src/App.vue
@@ -1,15 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <input v-model="userInfo.userId" type="text">
+    <input v-model="userInfo.userPw" type="text">
+    <LoginComponent v-bind="userInfo"></LoginComponent>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import LoginComponent from './components/LoginComponent.vue'
+
+let userInfo = {
+  userId : '',
+  userPw : ''
+}
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LoginComponent
+  },
+  data(){
+    return {
+      userInfo
+    }
   }
 }
 </script>
---
+++ Web/my-front/src/components/LoginComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        {{ userId }}
+        <br>
+        {{ userPw }}
+    </div>
+</template>
+
+
+<script>
+export default {
+    name:'LoginComponent',
+    props:['userId', 'userPw']
+}
+</script>
